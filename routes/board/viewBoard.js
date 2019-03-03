1c7449ac39441eacabb1dbbd9c217e967aa2cb34
wisemuji <wisemuji@gmail.com>
2019-03-04 01:49:10 +0900

    게시판 뷰

--- routes/board/viewBoard.js
+++ routes/board/viewBoard.js
@@ -2,12 +2,13 @@ import rndstring from 'randomstring';
 import moment from 'moment';
 
 module.exports = (app, Boards, Comments) => {
-    app.post('/viewBoard', async(req, res) => {
-            let board = new Boards(req.body);
-            let result = await Boards.findOne({ token: board.token })
+    app.get('/viewBoard/:token', async(req, res) => {
+            let result = await Boards.findOne({ token: req.params.token })
             if (!result) return res.status(404).json({ message: "Board Not Found" })
 
-            return res.status(200).json(result);
+            // return res.status(200).json(result);
+            res.render('qa_view', { item: result });
+
         })
         .get('/qa', (req, res) => {
             Boards.find({}).sort({ date: -1 }).exec(function(err, rawContents) { //오래된 순으로 정렬
