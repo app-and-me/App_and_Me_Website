1812499e48c3fa672cf448cc42a5c62aa4926f05
wisemuji <wisemuji@gmail.com>
2019-03-06 15:54:38 +0900

    로그 삭제

--- routes/board/viewBoard.js
+++ routes/board/viewBoard.js
@@ -13,9 +13,7 @@ module.exports = (app, Boards, Comments) => {
         .get('/qa', async(req, res) => {
             let page = req.query.page;
             if (!page) page = 1;
-            console.log(page);
             await Boards.count({}, function(err, c) {
-                console.log('Count is ' + c);
                 Boards.find().skip((page - 1) * 10).limit(10).sort({ date: -1 }).exec(function(err, rawContents) { //최신 순으로 정렬
                     if (err) throw err;
                     res.render('qa', { contents: rawContents, moment: moment, page: page, leng: c });
