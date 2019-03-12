baa344357ed5975c573143283a46524cba021cff
wisemuji <wisemuji@gmail.com>
2019-03-12 20:27:50 +0900

    노출된 비밀 코드 해결

--- routes/apply/vertify.js
+++ routes/apply/vertify.js
@@ -1,9 +1,16 @@
+import config from '../../config';
+
 module.exports = (app, Appliers) => {
     app.post('/vertify', async(req, res) => {
-        var result = await Appliers.find();
+        // var result = await Appliers.find().sort({ student_id: -1 });
         // res.status(200).json(result)
         var code = req.body.code;
-        if (code == 'appme678') res.render('vertify', { appliers: result });
-        else res.send('<script type="text/javascript">alert("앱앤미 부원이 아닙니다!!"); history.back();</script>');
+        await Appliers.count({}, function(err, c) {
+            Appliers.find().sort({ student_id: 1 }).exec(function(err, rawContents) { //학번 순으로 정렬
+                if (err) throw err;
+                if (code == config.code) res.render('vertify', { appliers: rawContents });
+                else res.send('<script type="text/javascript">alert("앱앤미 부원이 아닙니다!!"); history.back();</script>');
+            });
+        });
     })
 }
