e159d749390fb011e0963767b019dd357f50fe13
wisemuji <wisemuji@gmail.com>
2019-03-16 23:59:07 +0900

    코드 수정

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
     'PORT': '3000',
     'secret': 'iloveappandme',
+    'code': 'happyappme',
     'mongo_address': 'mongodb://appandme:doqrmflrhsk678@ds341825.mlab.com:41825/app-and-me'
 }
