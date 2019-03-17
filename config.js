7da9b3727fcfa514ddb90b6c0eb68da78f552685
wisemuji <wisemuji@gmail.com>
2019-03-17 23:57:59 +0900

    포트 번호 9000

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'PORT': '3000',
+    'PORT': '9000',
     'secret': 'iloveappandme',
     'code': 'happyappme',
     'mongo_address': 'mongodb://appandme:doqrmflrhsk678@ds341825.mlab.com:41825/app-and-me'
