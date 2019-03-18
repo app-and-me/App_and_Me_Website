30dc80d740c6badb51560dac877172771f5c742f
wisemuji <wisemuji@gmail.com>
2019-03-19 00:26:23 +0900

    주석제거

--- routes/apply/vertify.js
+++ routes/apply/vertify.js
@@ -2,8 +2,6 @@ import config from '../../config';
 
 module.exports = (app, Appliers) => {
     app.post('/vertify', async(req, res) => {
-        // var result = await Appliers.find().sort({ student_id: -1 });
-        // res.status(200).json(result)
         var code = req.body.code;
         await Appliers.count({}, function(err, c) {
             Appliers.find().sort({ student_id: 1 }).exec(function(err, rawContents) { //학번 순으로 정렬
